fd5964e241519f84dc7ebcd431fc074742a30c28
Naomi Sano <98775797+nsano1@users.noreply.github.com>
2023-03-22 21:38:57 -0700

    Update project2.cc

--- project2.cc
+++ project2.cc
@@ -32,43 +32,7 @@ vector<string> Nonterminals;
 vector<int> indexLHS;
 vector<int> indexRHS;
 vector<string> indexList; 
-
-/*
-Rule parseRule(string ruleInput) {
-    //add LHS and RHS onto respective vectors
-    //read input up to ARROW and make that LHS
-    Rule rule;
-    
-    //lexical analyzer's token string = ruleInput
-    lexRule->GetToken().lexeme = ruleInput;
-
-    
-
-    //get {LEFTHAND}
-    rule.leftHand = lexRule->GetToken();
-    std::cout << rule.leftHand.lexeme << endl;
-    //add to nonterminal directly, anything in lefthand will be nonterminal
-    Nonterminals.push_back(rule.leftHand.lexeme);
-    
-    //get {ARROW}
-    //inputRule->GetToken();
-
-    //get {RIGHTHAND}
-    while (lexRule->peek(1).token_type != END_OF_FILE){
-        if(lexRule->peek(1).token_type != ARROW) {
-            rule.rightHand.push_back(lexRule->GetToken());
-        }
-        else {
-            lexRule->GetToken();
-        }
-    }
-
-    //add the final token into RHS because the while loop will stop before reading the last token
-    rule.rightHand.push_back(lexRule->GetToken());
-    
-    //now return rule
-    return rule;
-}*/
+vector<string> sortedNonterminals;
 
 int index(string target) {
     for(int i = 0; i < indexList.size(); i++) {
@@ -79,70 +43,19 @@ int index(string target) {
     return -1;
 }
 
-// read grammar
-void ReadGrammar()
-{
-    indexList.push_back("#");
-    indexList.push_back("$");
-
-    Rule thisRule;
-
-    bool endStar = false;
-
-    while(true){ //for each rule in the set of rules
-
-        //first get token to analyze it
-        Token iterator = lexGrammar->GetToken();
-
-        //if hash, end of grammar
-        if (iterator.token_type == HASH){
-            break;
-        }
-        //if arrow, skip
-        else if (iterator.token_type == ARROW){
-            continue;
-        }
-        //if id, add to left hand side
-        else if (iterator.token_type == ID){
-            // if(index(lexGrammar->peek(1).lexeme) == -1) {
-            //     indexList.push_back(lexGrammar->peek(1).lexeme);    
-            // }
-
-            //get left hand side
-            thisRule.leftHand = iterator;
-
-            //now get right hand side (a list)
-            while (true) { //for each RHS in rule
-
-                //get token to analyze it
-                Token token = lexGrammar->GetToken();
-                //if  star, end of rule
-                if (token.token_type == STAR) {
-                    break;
-                }
-                //if id, add to RHS
-                else if (token.token_type == ID) {
-                    // if(index(lexGrammar->peek(1).lexeme) == -1) {
-                    //     indexList.push_back(lexGrammar->peek(2).lexeme);    
-                    // }
-                    thisRule.rightHand.push_back(token);
-                }
-            }
-            //reset
-            endStar = false;
-        }
-        ruleList.push_back(thisRule);
-    }
-
-
-    //make the indexLists
-    
-}
-
 //tells if it is an element in given list
 bool isInNonterminals(string target) {
     for(int i = 0; i < Nonterminals.size(); i++) {
         if(Nonterminals[i] == target) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isElement(string target, vector<Rule> list) {
+    for(int i = 0; i < list.size(); i ++) {
+        if(list[i].leftHand.lexeme == target) {
             return true;
         }
     }
@@ -167,17 +80,71 @@ bool isInTerminal(string target) {
     return false;
 }
 
-// Task 1
-void printTerminalsAndNoneTerminals()
+
+// read grammar
+void ReadGrammar()
 {
+    indexList.push_back("#");
+    indexList.push_back("$");
 
-    std::cout << "1\n";
+    Rule thisRule;
 
-    //{NONTERMINAL} --> {TERMINAL}*
-    //----formatting
-    //Terminals first, then nontemrinals are printed all in one line
+    bool endStar = false;
+    //read grammar token by token
+    while(true){ //for each rule in the set of rules
 
-    //  ----make nonterminal list - easy just make sure no repeats----
+        //first get token to analyze it
+        Token iterator = lexGrammar->GetToken();
+
+        //if hash, end of grammar
+        if (iterator.token_type == HASH){
+            break;
+        }
+        //if arrow, skip
+        else if (iterator.token_type == ARROW){
+            continue;
+        }
+        //if id, add to left hand side
+        else if (iterator.token_type == ID){
+
+            //index
+            if(index(iterator.lexeme) == -1) {
+                indexList.push_back(iterator.lexeme);    
+            }
+
+            //get left hand side
+            thisRule.leftHand = iterator;
+
+            //now get right hand side (a list)
+            while (true) { //for each RHS in rule
+
+                //get token to analyze it
+                Token token = lexGrammar->GetToken();
+                //if  star, end of rule
+                if (token.token_type == STAR) {
+                    break;
+                }
+                //if id, add to RHS
+                else if (token.token_type == ID) {
+
+                    //index
+                    if(index(token.lexeme) == -1) {
+                        indexList.push_back(token.lexeme);    
+                    }
+                    thisRule.rightHand.push_back(token);
+                }
+            }
+            //reset
+            endStar = false;
+        }
+        ruleList.push_back(thisRule);
+    }
+
+    //making terminals and nonterminals 
+        //{NONTERMINAL} --> {TERMINAL}*
+        //----formatting
+        //Terminals first, then nontemrinals are printed all in one line
+        //  ----make nonterminal list - easy just make sure no repeats----
 
     //first add the first LHS into non terminals
     Nonterminals.push_back(ruleList[0].leftHand.lexeme);
@@ -195,56 +162,36 @@ void printTerminalsAndNoneTerminals()
     for(int i = 0; i < ruleList.size(); i++) { //for each rule's right hand
 
         for(int j = 0; j < ruleList[i].rightHand.size(); j++) { //for each right hand id
+            
             //if this RHS is not a nonterminal, add to terminals and make sure no repeat
-            if(!isInNonterminals(ruleList[i].rightHand[i].lexeme) && !isInTerminal(ruleList[i].rightHand[i].lexeme)) {
-                Terminals.push_back(ruleList[i].rightHand[i].lexeme);
+            if(!isInNonterminals(ruleList[i].rightHand[j].lexeme) && !isInTerminal(ruleList[i].rightHand[j].lexeme)) {
+                Terminals.push_back(ruleList[i].rightHand[j].lexeme);
             }
             //if in nonterminals, is a nonterminal
         }
     }
     //  ----we now have complete terminals list (no repeats)----
 
+    //INDEX METHOD for sorting
+    for(int i = 0; i < indexList.size(); i++) {
+        //if index[i] is a nonTerminal and not in the sortedTerminals, add it to the sortedNonTerminals
+        if(!(isElement(indexList[i], sortedNonterminals)) && isInNonterminals(indexList[i])) {
+            sortedNonterminals.push_back(indexList[i]);
+        }
+    }
     
-    //  ----SORT----
-    //sort the terminal and nonTerminal list to be in order as they appear in the grammar
-    //create sorted lists
-    vector<string> sortedTerminals;
-    vector<string> sortedNonterminals;
+}
 
-    //go each rule in the grammar
-    for(int i = 0; i < ruleList.size(); i++) {
-        //go through LHS first
-        //if LHS is in terminal AND not in sortedNonterminals
-        if(!isInTerminal(ruleList[i].leftHand.lexeme) && !(isElement(ruleList[i].leftHand.lexeme, sortedNonterminals))) {
-            
-            sortedNonterminals.push_back(ruleList[i].leftHand.lexeme);
-        }
-        //go through RHS second
-        for(int j = 0; j < ruleList[i].rightHand.size(); j++) {
-            if(isInTerminal(ruleList[i].rightHand[j].lexeme)  && !(isElement(ruleList[i].rightHand[j].lexeme, sortedTerminals))) {
-                sortedTerminals.push_back(ruleList[i].rightHand[j].lexeme);
-            }
-            else if(!(isInTerminal(ruleList[i].rightHand[j].lexeme))  && !(isElement(ruleList[i].rightHand[j].lexeme, sortedNonterminals))){
-                sortedNonterminals.push_back(ruleList[i].rightHand[j].lexeme);
-            }
-        }
-    }
-    //print out the sorted lists //not sorted correctly yet
-    for (int i = 0; i < sortedTerminals.size(); i++) {
-        cout << sortedTerminals[i];
-        if (i != sortedTerminals.size() - 1) {
-            cout << " ";
-        }
-    }
-    cout << " "; // add a space here
-    for (int i = 0; i < sortedNonterminals.size(); i++) {
-        cout << sortedNonterminals[i];
-        if (i != sortedNonterminals.size() - 1) {
-            cout << " ";
-        }
+// Task 1
+void printTerminalsAndNoneTerminals()
+{
+    for(int i = 0; i < Terminals.size(); i++) {
+        cout << Terminals[i] << " ";
     }
 
-    cout << endl << flush;
+    for(int i = 0; i < sortedNonterminals.size(); i++) {
+        cout << sortedNonterminals[i] << " ";
+    }
 }
 
 bool rhsTrue(bool generating[], vector<Token> rhs) {
@@ -256,8 +203,6 @@ bool rhsTrue(bool generating[], vector<Token> rhs) {
     return true;
 }
 
-
-
 // Task 2
 void RemoveUselessSymbols()
 {
@@ -266,10 +211,7 @@ void RemoveUselessSymbols()
 
     //remove any unaccessible rules S->AB, C->c, A->a, B->b: remove C->c
 
-    std::cout << "2\n";
-
     bool generating[indexList.size()];
-
 
     //set all terminals to generating
     for(int i = 0; i < indexList.size(); i++) {
@@ -278,14 +220,19 @@ void RemoveUselessSymbols()
         }
     }
 
+    bool change = true;
 
-    //go through each rule and check if the RHS is all generating
-    //if it is change the LHS variable to True
-    for(int i = 0; i < ruleList.size(); i++) {//goes through each rule
-
-        for(int j = 0; j < ruleList[i].rightHand.size(); j++) { //go through the RHS of each rule
-            if(rhsTrue(generating, ruleList[i].rightHand)) { //if the full RHS is true set the left hadn side variable to true
-                generating[index(ruleList[i].leftHand.lexeme)] = true;
+    while(change) {
+        //go through each rule and check if the RHS is all generating
+        //if it is change the LHS variable to True
+        for(int i = 0; i < ruleList.size(); i++) {//goes through each rule
+            for(int j = 0; j < ruleList[i].rightHand.size(); j++) { //go through the RHS of each rule
+                if(rhsTrue(generating, ruleList[i].rightHand)) { //if the full RHS is true set the left hadn side variable to true
+                    generating[index(ruleList[i].leftHand.lexeme)] = true;
+                }
+                else{
+                    change = false;
+                }
             }
         }
     }
@@ -295,9 +242,22 @@ void RemoveUselessSymbols()
     vector<Rule> generatingRules;
     //go through the rule list and add any rules that are generating to the vector
     for(int i = 0; i < ruleList.size(); i++) {
-        if(generating[index(ruleList[i].leftHand.lexeme)] == true && rhsTrue(generating, ruleList[i].rightHand)) {
-            generatingRules.push_back(ruleList[i]);
+        if(generating[index(ruleList[i].leftHand.lexeme)]) {
+            cout << "HERE1" << endl;
         }
+        if(isElement(ruleList[i].leftHand.lexeme, generatingRules)) {
+            cout << "HERE2" << endl;
+        }
+        //if it is a generating rule, add it to the generatingRuleList && check for repeats
+        if(generating[index(ruleList[i].leftHand.lexeme)] == true && 
+            isElement(ruleList[i].leftHand.lexeme, generatingRules)) {
+                generatingRules.push_back(ruleList[i]);
+        }
+    }
+
+    //print out generating rules
+    for(int i = 0; i < generatingRules.size(); i ++) {
+        cout << i << " " << generatingRules[i].leftHand.lexeme << endl;
     }
 
     //caluclate reachable symbols
@@ -321,9 +281,12 @@ void RemoveUselessSymbols()
             reachableRules.push_back(generatingRules[i]);
         }
     }
+
+
     cout << "going to print" << endl;
     //print out the reachableRule vector
     for(int i = 0; i < reachableRules.size(); i++) {
+        cout << "forloop" << endl;
         //print LHS
         cout << reachableRules[i].leftHand.lexeme << " "; 
         //print ARROW
